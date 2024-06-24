fcb61c23ba485cf8e6eb00657686cbb20dab7fe1
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-06-24 12:16:29 +0300

    formatting FileNotAvailable file

--- src/ItemNotAvailableException.java
+++ src/ItemNotAvailableException.java
@@ -1,7 +1,5 @@
 public class ItemNotAvailableException extends Exception {
     public ItemNotAvailableException(String message) {
-
         System.out.println("item is not available.");
-
     }
 }
